5910972e70f1de7f092cf416f21468c384ca4e5b
amrabdelhameed1001 <85768785+amrabdelhameed1001@users.noreply.github.com>
2022-05-31 22:24:10 +0200

    Add files via upload

---
+++ swapping variables with xor.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+
+ /*write your solution here. don't creat any other variables and use only xor*/
+
+	printf("%d %d", b, a);
+	return 0;
+}
